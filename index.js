3ef87d2f0a528e88f3015dd3996a1ffca66375a7
hsaidahmed <hashim.saidahmed@gmail.com>
2021-12-06 02:01:07 +0000

    ISS3

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // const { fetchMyIP } = require('./iss');
 
+const { fetchISSFlyOverTimes } = require('./iss');
 
-const { fetchCoordsByIP } = require('./iss');
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
 
-fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
--- iss.js
+++ iss.js
@@ -25,25 +25,44 @@ const request = require('request');
 //   });
 // };
 
-const fetchCoordsByIP = function(ip, callback) {
-  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+// const fetchCoordsByIP = function(ip, callback) {
+//   request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+//     if (error) {
+//       callback(error, null);
+//       return;
+//     }
+
+//     if (response.statusCode !== 200) {
+//       callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+//       return;
+//     }
+
+//     const { latitude, longitude } = JSON.parse(body);
+
+//     callback(null, { latitude, longitude });
+//   });
+// };
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
     }
 
     if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
       return;
     }
 
-    const { latitude, longitude } = JSON.parse(body);
-
-    callback(null, { latitude, longitude });
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
   });
 };
 
+// Don't need to export the other functions since we are not testing them right now.
+module.exports = { fetchISSFlyOverTimes };
+
 // Don't need to export the other function since we are not testing it right now.
-
-
-module.exports = { fetchCoordsByIP };
